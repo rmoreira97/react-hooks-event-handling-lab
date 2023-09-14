36157c51bbea9df7f7fe36a334dce35fd65abd5c
Rafael Moreira <rafa.siffert@gmail.com>
2023-09-14 11:13:05 -0400

    finished

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const HandleInputPassword = (event) => {
+    console.log("Entering password...", event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={HandleInputPassword} />
+    </div>
+  );
 }
 
 export default Keypad;
